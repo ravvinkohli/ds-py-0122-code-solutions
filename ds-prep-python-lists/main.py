e3408efddd0147adb4711e7aacd14b3c16798d26
Ravleen Kaur Kohli <ravleen.chemistry@gmail.com>
2022-01-15 11:50:34 -0800

    Changes made

--- ds-prep-python-lists/main.py
+++ ds-prep-python-lists/main.py
@@ -4,15 +4,15 @@ print(ages[4])
 print(ages[-2])
 print(ages[2:6])
 print(ages[:4])
-print("17" in ages)
-print("42" in ages)
-ages[2] = "18"
+print(17 in ages)
+print(42 in ages)
+ages[2] = 18
 print(ages)
-ages[4] = "temp."
+temp = ages[4]
 ages [4] = ages[3]
-ages[3] = 32
+ages[3] = temp
 print(ages)
-ages.append("45")
+ages.append(45)
 ages.insert(0 , 32)
 ages.insert(5 , 37)
 print(ages)
