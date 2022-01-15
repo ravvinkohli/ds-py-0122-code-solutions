8238313f53ddbd7954f71fc2a23dca9f77b961e5
Ravleen Kaur Kohli <ravleen.chemistry@gmail.com>
2022-01-15 12:26:10 -0800

    Changes made - conditionals

--- ds-prep-python-conditionals/main.py
+++ ds-prep-python-conditionals/main.py
@@ -1,37 +1,42 @@
 day_of_week = "friday"
-if day_of_week == "saturday" or "sunday":
+if day_of_week == "saturday" or day_of_week == "sunday":
   print("have a good weekend")
 else:
   print("it is a weekday")
 student_1_score = 85
-pass_or_fail = student_1_score
 if student_1_score >= 70:
-  print(student_1_score , "You passed!")
+  pass_or_fail = "You passed!"
 else:
-  print(student_1_score , "You failed!")
+  pass_or_fail = "You Failed!"
+print(pass_or_fail , student_1_score)
 student_2_score = 100
 if student_2_score < 60:
-  print(student_2_score , "F")
+  letter_grade = "F"
 elif student_2_score >= 60 and student_2_score < 70:
-  print(student_2_score , "D")
+  letter_grade = "D"
 elif student_2_score >= 70 and student_2_score < 80:
-  print(student_2_score , "C")
+  letter_grade = "C"
 elif student_2_score >= 80 and student_2_score < 90:
-  print(student_2_score , "B")
+  letter_grade = "B"
 elif student_2_score >= 90 and student_2_score < 100:
-  print(student_2_score , "A")
+  letter_grade = "A"
 else:
-  print(student_2_score , "A+")
-def get_season_info(summer):
-  ("Statistically, it's likely to be hotter today than in 6 months from now. Don't sweat it, though.") == summer
-  return summer
-print(get_season_info)
-elif get_season_info == spring:
-  print("The flowers are blooming while it's spring, but that correlation, not causation.")
-elif get_season_info == autumn:
-  print("The leaves seem to regress to warmer colors as autumn approaches its end.")
-elif get_season_info == winter:
-  print("There may only be a high likelihood of it being cold today, but there's a 100 percent chance of me wanting that sweater.")
-else:
-  print("That's not a season. Most likely.")
-print((get_season_info(summer)))
+  letter_grade = "A+"
+print(letter_grade , student_2_score)
+def get_season_info(season):
+  if season == "summer":
+    return "Statistically, it's likely to be hotter today than in 6 months from now. Don't sweat it, though."
+  elif season == "spring":
+    return "The flowers are blooming while it's spring, but that correlation, not causation."
+  elif season == "autumn":
+    return "The leaves seem to regress to warmer colors as autumn approaches its end."
+  elif season == "winter":
+    return "There may only be a high likelihood of it being cold today, but there's a 100 percent chance of me wanting that sweater."
+  else:
+    return "That's not a season. Most likely."
+print(get_season_info("summer"))
+print(get_season_info("spring"))
+print(get_season_info("autumn"))
+print(get_season_info("winter"))
+age = 42
+print("adult") if age >= 18 else print("child")
