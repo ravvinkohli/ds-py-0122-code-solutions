ffbaf6d3f4a50ae621feb0e5f8cfe56ab8a1dadc
Ravleen Kaur Kohli <ravleen.chemistry@gmail.com>
2022-01-15 13:05:25 -0800

    Corrected

--- ds-prep-python-variables/main.py
+++ ds-prep-python-variables/main.py
@@ -1,4 +1,4 @@
-from types import ClassMethodDescriptorType
+from turtle import heading
 
 
 first_name = 'ravleen'
@@ -6,31 +6,25 @@ last_name = "kaur"
 full_name = first_name + " " + last_name
 print(full_name)
 height_in_inches = 66
-x = type(height_in_inches)
-print(height_in_inches)
-print(x)
-y = height_in_inches_float = 66.0
-print(y)
-a = height_in_centimeters_float = y * 2.54
-b = height_in_meters = a / 100
-print(b)
+print(type(height_in_inches) , height_in_inches)
+height_in_inches_float = float(height_in_inches)
+print(type(height_in_inches_float) , height_in_inches_float)
+height_in_centimeters_float = height_in_inches_float * 2.54
+height_in_meters = height_in_centimeters_float / 100
+print(height_in_meters)
 eats_plants = True
 eats_animals = False
-is_animal = eats_plants or eats_animals
-is_omnivore = eats_plants and eats_animals
+is_animal = eats_plants == True or eats_animals == True
+is_omnivore = eats_plants == True and eats_animals ==True
 is_plant = not(is_animal)
 print(is_animal , is_omnivore , is_plant)
-print(eats_plants or eats_animals)
-print(not(eats_plants or eats_animals))
-v = is_omnivore
 mean_height_in_meters = 1.7155
 short_threshold_in_meters = 1.576
 tall_threshold_in_meters = 1.860
-print(height_in_meters == mean_height_in_meters)
 is_mean_height = height_in_meters == mean_height_in_meters
 is_short = height_in_meters < short_threshold_in_meters
 is_tall = height_in_meters >= tall_threshold_in_meters
 is_normal_height = height_in_meters >= short_threshold_in_meters and height_in_meters < tall_threshold_in_meters
 print (is_mean_height , is_short , is_tall , is_normal_height)
 nothing = None
-print(nothing , type)
+print(type(nothing) , nothing)
