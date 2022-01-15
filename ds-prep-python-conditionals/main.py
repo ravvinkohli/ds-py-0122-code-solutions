35cfc5434e8ab96406e2e09dc8f466de0767de84
Ravleen Kaur Kohli <ravleen.chemistry@gmail.com>
2022-01-14 21:50:34 -0800

    Completed ds-prep-python-conditionals

---
+++ ds-prep-python-conditionals/main.py
@@ -0,0 +1,37 @@
+day_of_week = "friday"
+if day_of_week == "saturday" or "sunday":
+  print("have a good weekend")
+else:
+  print("it is a weekday")
+student_1_score = 85
+pass_or_fail = student_1_score
+if student_1_score >= 70:
+  print(student_1_score , "You passed!")
+else:
+  print(student_1_score , "You failed!")
+student_2_score = 100
+if student_2_score < 60:
+  print(student_2_score , "F")
+elif student_2_score >= 60 and student_2_score < 70:
+  print(student_2_score , "D")
+elif student_2_score >= 70 and student_2_score < 80:
+  print(student_2_score , "C")
+elif student_2_score >= 80 and student_2_score < 90:
+  print(student_2_score , "B")
+elif student_2_score >= 90 and student_2_score < 100:
+  print(student_2_score , "A")
+else:
+  print(student_2_score , "A+")
+def get_season_info(summer):
+  ("Statistically, it's likely to be hotter today than in 6 months from now. Don't sweat it, though.") == summer
+  return summer
+print(get_season_info)
+elif get_season_info == spring:
+  print("The flowers are blooming while it's spring, but that correlation, not causation.")
+elif get_season_info == autumn:
+  print("The leaves seem to regress to warmer colors as autumn approaches its end.")
+elif get_season_info == winter:
+  print("There may only be a high likelihood of it being cold today, but there's a 100 percent chance of me wanting that sweater.")
+else:
+  print("That's not a season. Most likely.")
+print((get_season_info(summer)))
